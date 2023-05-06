e1e576814f9af913792d667042d6fb3382232501
Stella <stellayangfan@gmail.com>
2023-05-06 16:10:43 +0800

    feat(compiler-core/baseCompile): init compiler core

---
+++ packages/compiler-core/src/compile.ts
@@ -0,0 +1,31 @@
+import { generate } from "./codegen"
+import { baseParse } from "./parse"
+import { transform } from "./transform"
+
+export function baseCompile(template) {
+  // 1. 将模板转化成 ast 模板
+  const ast = baseParse(template)
+  // 2. 对 ast 语法树进行转化
+  transform(ast)
+  // 生成代码
+  return generate(ast)
+}
+
+// 准备语法树相关type
+export const enum NodeTypes {
+  ROOT,
+  ELEMENT,
+  TEXT,
+  COMMENT,
+  SIMPLE_EXPRESSION,
+  INTERPOLATION, // template expression
+  ATTRIBUTE,
+  DIRECTIVE,
+  COMPOUND_EXPRESSION,
+  IF,
+  IF_BRANCH,
+  FOR,
+  TEXT_CALL,
+  VNODE_CALL,
+  JS_CALL_EXPRESSION
+}
