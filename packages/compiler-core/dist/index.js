db375adb590256520bbadf32effea4853988d24b
Stella <stellayangfan@gmail.com>
2023-05-08 17:15:23 +0800

    feat(compiler-core/baseCompile): support parseInterpolation

--- packages/compiler-core/dist/index.js
+++ packages/compiler-core/dist/index.js
@@ -22,7 +22,7 @@ function parseChildren(context) {
     const s = context.source;
     let node;
     if (s.startsWith("{{")) {
-      node = {};
+      node = parseInterpolation(context);
     } else if (s[0] === "<") {
       node = {};
     }
@@ -61,30 +61,63 @@ function parseTextData(context, endIndex) {
   advanceBy(context, endIndex);
   return rawText;
 }
-function advanceBy(context, endIndex) {
+function advanceBy(context, numberOfCharacters) {
   const s = context.source;
-  advancePositionWithMutation(context, s, endIndex);
-  context.source = s.slice(endIndex);
+  advancePositionWithMutation(context, s, numberOfCharacters);
+  context.source = s.slice(numberOfCharacters);
 }
-function advancePositionWithMutation(context, s, endIndex) {
+function advancePositionWithMutation(pos, source, numberOfCharacters) {
   let linesCount = 0;
   let linePos = -1;
-  for (let i = 0; i < endIndex; i++) {
-    if (s.charCodeAt(i) === 10) {
+  for (let i = 0; i < numberOfCharacters; i++) {
+    if (source.charCodeAt(i) === 10) {
       linesCount++;
       linePos = i;
     }
   }
-  context.offset += endIndex;
-  context.line += linesCount;
-  context.column = linePos === -1 ? context.column + endIndex : endIndex - linePos;
+  pos.offset += numberOfCharacters;
+  pos.line += linesCount;
+  pos.column = linePos === -1 ? pos.column + numberOfCharacters : numberOfCharacters - linePos;
+  return pos;
 }
-function getSelection(context, start) {
-  const end = getCursor(context);
+function getSelection(context, start, end) {
+  end = end || getCursor(context);
   return {
     start,
     end,
     source: context.originalSource.slice(start.offset, end.offset)
+  };
+}
+function parseInterpolation(context) {
+  const [open, close] = ["{{", "}}"];
+  const closeIndex = context.source.indexOf(close, open.length);
+  if (closeIndex === -1) {
+    return void 0;
+  }
+  const start = getCursor(context);
+  advanceBy(context, open.length);
+  const innerStart = getCursor(context);
+  const innerEnd = getCursor(context);
+  const rawContentLength = closeIndex - open.length;
+  const rawContent = context.source.slice(0, rawContentLength);
+  const preTrimContent = parseTextData(context, rawContentLength);
+  const content = preTrimContent.trim();
+  const startOffset = preTrimContent.indexOf(content);
+  if (startOffset > 0) {
+    advancePositionWithMutation(innerStart, rawContent, startOffset);
+  }
+  const endOffset = rawContentLength - (preTrimContent.length - content.length - startOffset);
+  advancePositionWithMutation(innerEnd, rawContent, endOffset);
+  advanceBy(context, close.length);
+  return {
+    type: 5 /* INTERPOLATION */,
+    content: {
+      type: 4 /* SIMPLE_EXPRESSION */,
+      isStatic: false,
+      content,
+      loc: getSelection(context, innerStart, innerEnd)
+    },
+    loc: getSelection(context, start)
   };
 }
 
@@ -95,6 +128,7 @@ function transform(root) {
 // packages/compiler-core/src/compile.ts
 function baseCompile(template) {
   const ast = baseParse(template);
+  console.log(ast);
   transform(ast);
   return generate(ast);
 }
--- packages/compiler-core/src/parse.ts
+++ packages/compiler-core/src/parse.ts
@@ -1,5 +1,7 @@
 import { NodeTypes } from "./compile"
 
+
+
 export interface ParserContext {
   readonly originalSource: string
   source: string
@@ -37,6 +39,30 @@ export interface TextNode extends Node {
   content: string
 }
 
+export interface SimpleExpressionNode extends Node {
+  type: NodeTypes.SIMPLE_EXPRESSION
+  content: string
+  isStatic: boolean
+}
+export interface CompoundExpressionNode extends Node {
+  type: NodeTypes.COMPOUND_EXPRESSION
+  children: (
+    | SimpleExpressionNode
+    | CompoundExpressionNode
+    | InterpolationNode
+    | TextNode
+    | string
+    | symbol
+  )[]
+}
+
+export type ExpressionNode = SimpleExpressionNode | CompoundExpressionNode
+
+export interface InterpolationNode extends Node {
+  type: NodeTypes.INTERPOLATION
+  content: ExpressionNode
+}
+
 export type TemplateChildNode =
   | TextNode
 
@@ -60,7 +86,7 @@ function parseChildren(
     let node
     if (s.startsWith('{{')) {
       // TODO: process expression
-      node = {}
+      node = parseInterpolation(context)
     } else if (s[0] === '<') {
       // TODO: process element
       node = {}
@@ -121,43 +147,85 @@ function parseTextData(
 
 function advanceBy(
   context: ParserContext,
-  endIndex: number
+  numberOfCharacters: number
 ): void {
   const s = context.source
-  advancePositionWithMutation(context, s, endIndex)
-  context.source = s.slice(endIndex)
+  advancePositionWithMutation(context, s, numberOfCharacters)
+  context.source = s.slice(numberOfCharacters)
 }
 
 // update context
 function advancePositionWithMutation(
-  context: ParserContext,
-  s: string,
-  endIndex: number
-): void {
+  pos: Position,
+  source: string,
+  numberOfCharacters: number
+): Position {
   let linesCount = 0
   let linePos = -1
-  for (let i = 0; i < endIndex; i++) {
-    if (s.charCodeAt(i) === 10) { // \n new line
+  for (let i = 0; i < numberOfCharacters; i++) {
+    if (source.charCodeAt(i) === 10) { // \n new line
       linesCount++
       linePos = i
     }
   }
-  context.offset += endIndex
-  context.line += linesCount
+  pos.offset += numberOfCharacters
+  pos.line += linesCount
+  pos.column = linePos === -1
+    ? pos.column + numberOfCharacters
+    : numberOfCharacters - linePos
 
-  context.column = linePos === -1
-    ? context.column + endIndex
-    : endIndex - linePos
+  return pos
 }
 
 function getSelection(
   context: ParserContext,
-  start: Position
+  start: Position,
+  end?: Position
 ): SourceLocation {
-  const end = getCursor(context)
+  end = end || getCursor(context)
   return {
     start,
     end,
     source: context.originalSource.slice(start.offset, end.offset)
+  }
+}
+
+function parseInterpolation(
+  context: ParserContext
+): InterpolationNode | undefined {
+  // {{state.name}}
+  const [open, close] = ['{{', '}}']
+  const closeIndex = context.source.indexOf(close, open.length)
+
+  if (closeIndex === -1) {
+    // EmitError
+    return undefined
+  }
+
+  const start = getCursor(context)
+  advanceBy(context, open.length)
+  const innerStart = getCursor(context)
+  const innerEnd = getCursor(context)
+  const rawContentLength = closeIndex - open.length
+  const rawContent = context.source.slice(0, rawContentLength)
+  const preTrimContent = parseTextData(context, rawContentLength)
+  const content = preTrimContent.trim()
+  const startOffset = preTrimContent.indexOf(content)
+  if (startOffset > 0) {
+    advancePositionWithMutation(innerStart, rawContent, startOffset)
+  }
+  const endOffset = rawContentLength - (preTrimContent.length - content.length - startOffset)
+  advancePositionWithMutation(innerEnd, rawContent, endOffset)
+  advanceBy(context, close.length)
+
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      isStatic: false,
+      content,
+      loc: getSelection(context, innerStart, innerEnd)
+    },
+    loc: getSelection(context, start)
   }
 }
